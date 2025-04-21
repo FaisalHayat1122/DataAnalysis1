13e7bc063aa4abf3b421e8331f070967f31a384a
Faisal Hayat <168349740+FaisalHayat1122@users.noreply.github.com>
2025-04-21 09:34:20 -0700

    Create 5th_class.py

---
+++ 5th_class.py
@@ -0,0 +1,25 @@
+'''
+num1 = int(input())
+while num1<10:
+    print(num1)
+    #num1+=1
+    num1=num1+1
+'''
+'''
+table=int(input("Enter a nmber of table"))
+nu1 = int(input("Enter a value:"))
+while num1<=10:
+    print(table,"x",num1,"=",num1*table)
+    num1=num1+1
+'''
+
+table=int(input("Enter a nmber of table"))
+num1 = int(input("Enter a value:"))
+while True:
+    print(table,"x",num1,"=",num1*table)
+    if num1==10:
+        break
+    num1=num1+1
+    #break ,continue (return)
+
+
