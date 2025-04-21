1814ce9360fc6fcfc934df2d867d616db219fbdd
Faisal Hayat <168349740+FaisalHayat1122@users.noreply.github.com>
2025-04-21 09:32:26 -0700

    Create 4th_class.py

---
+++ 4th_class.py
@@ -0,0 +1,40 @@
+'''
+size = int(input("Enter your size"))
+
+if size <= 10:
+    print("size is less than 10, welcome to the casual shirt drawer.")
+    color = input("Enter a color of shirt in casual you want?")
+    if color =="black":
+        print("have your black casual shirt of size",size)
+    elif color=="red":
+        print("have your red color",size)
+    elif color=="white":
+        print("have your white color",size)
+        
+else:
+    print("invalid")
+'''
+'''
+pers=input("Which personality do you like")
+
+if pers=="Quaid":
+    print("The age of Quaid is 100")
+    
+elif pers=="IK":
+    print("The age of IK is 70")
+    
+else:
+    print("personality not available")
+    
+'''
+pet=input("Which Animal Do You Like To Get..?")
+if pet=="Horse":
+    print("Sorry We don't have")
+elif pet == ("Dog"):
+       print("Get it")
+elif pet =="Cat":
+    print("Yes this is")
+    
+else:
+    print("Sorry You Should Go Another Store")
+
