e3010cc85ef6a507df7f470b9ec3a0067a736015
Faisal Hayat <168349740+FaisalHayat1122@users.noreply.github.com>
2025-04-21 09:30:40 -0700

    Create 2nd_class.py

---
+++ 2nd_class.py
@@ -0,0 +1,48 @@
+'''
+#         Task 1
+print(        "WelCome Sir"        )
+name1=input("whats your name sir")
+age1=int(input("How old are you"))
+price=int(input("what is your budget"))
+
+print("I Show You Something New It Is In Your Budget")
+
+if price >100:
+    print("sorry")
+
+    
+elif price == 120:
+    print( "sorry")
+    
+    
+else:
+    price == 100
+    print("This is your Gift")
+
+
+print("This is your Gift")
+'''
+print("Wellcome To My Store")
+name = input("Who are you...")
+age = int(input("How Old Are You"))
+size = int(input("How Long are you"))
+
+#print("I give you as you want")
+
+if size > 12 and size <14:
+    print("No")
+    
+elif size > 14 or size < 14:
+    print("Sorry")
+    
+elif size >=16  :
+    print("No I don't have")
+    
+elif size == 18:
+    print("Yes it is")
+    
+else:
+    print("This is for you")
+
+
+
